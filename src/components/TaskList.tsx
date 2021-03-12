8bd1ffbc7974cc1a7fee56ccc54358b9dd367d11
Carlos UchOa <carlosjuchoa@hotmail.com>
2021-03-12 16:39:16 -0300

    :tada: Desafio 01 Finalizado

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -16,14 +16,33 @@ export function TaskList() {
 
   function handleCreateNewTask() {
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+
+    if(!newTaskTitle) return;
+
+    const newTask = {
+        id: Math.random(),
+        title: newTaskTitle,
+        isComplete: false
+    }
+    setTasks(oldState => [...oldState, newTask]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
-  }
+
+    const newTasks = tasks.map(task => task.id == id ? {
+      ...task,
+      isComplete: !task.isComplete
+     } : task);
+      setTasks(newTasks)
+    }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+
+    const filteredTasks = tasks.filter(task => task.id != id);
+    setTasks(filteredTasks)
   }
 
   return (
